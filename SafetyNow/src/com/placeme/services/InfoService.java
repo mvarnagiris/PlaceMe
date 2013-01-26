f4936de2ec6be8a475183d25d43515ffbd032d62
Mantas <Mantas@Mantas-DELL>
2013-01-26 11:38:47 +0000

    Removed some stuff

---
+++ SafetyNow/src/com/placeme/services/InfoService.java
@@ -0,0 +1,17 @@
+package com.placeme.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class InfoService extends IntentService
+{
+	public InfoService()
+	{
+		super("");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent)
+	{
+	}
+}
