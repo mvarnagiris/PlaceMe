2f2ecb7c6876426ab5a11aca564ba084232003c2
Mantas <Mantas@Mantas-DELL>
2013-01-26 13:53:48 +0000

    Drawer is working

--- SafetyNow/src/com/placeme/ui/MainActivity.java
+++ SafetyNow/src/com/placeme/ui/MainActivity.java
@@ -12,13 +12,18 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 
 import com.placeme.R;
 import com.placeme.services.LocationService;
+import com.placeme.views.DrawerView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
 	public static final String	TAG	= MainActivity.class.getSimpleName();
+
+	private DrawerView			drawer_V;
 
 	private IntentFilter		mFilter;
 	private LocationReceiver	mReceiver;
@@ -26,9 +31,32 @@ public class MainActivity extends Activity {
 	private int					mLat, mLon;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Get views
+		drawer_V = (DrawerView) findViewById(R.id.drawer_V);
+		drawer_V.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener()
+		{
+			@Override
+			public void onGlobalLayout()
+			{
+
+				drawer_V.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+				// Opening this in a delayed runnable makes it look smoother.
+				drawer_V.postDelayed(new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						drawer_V.openDrawer(true);
+					}
+				}, 300);
+			}
+		});
+
 		mReceiver = new LocationReceiver();
 		mFilter = new IntentFilter(LocationService.ACTION_LOCATE_ME);
 
@@ -39,37 +67,45 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-	protected void onResume() {
+	protected void onResume()
+	{
 		super.onResume();
 		LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mReceiver, mFilter);
-		if ((-1 == mLat || -1 == mLon) && !isServiceRunning(LocationService.class)) {
-			startService(new Intent(getApplicationContext(), LocationService.class)
-					.setAction(LocationService.ACTION_LOCATE_ME));
+		if ((-1 == mLat || -1 == mLon) && !isServiceRunning(LocationService.class))
+		{
+			startService(new Intent(getApplicationContext(), LocationService.class).setAction(LocationService.ACTION_LOCATE_ME));
 		}
 	}
 
 	@Override
-	protected void onPause() {
+	protected void onPause()
+	{
 		super.onPause();
 		LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(mReceiver);
 	}
 
-	private class LocationReceiver extends BroadcastReceiver {
+	private class LocationReceiver extends BroadcastReceiver
+	{
 		@Override
-		public void onReceive(Context context, Intent intent) {
+		public void onReceive(Context context, Intent intent)
+		{
 			String action = intent.getAction();
-			if (LocationService.ACTION_LOCATE_ME.equals(action)) {
-				Log.d(TAG,
-						String.format("Got location %d, %d", intent.getIntExtra(LocationService.LAT, -1),
-								intent.getIntExtra(LocationService.LON, -1)));
+			if (LocationService.ACTION_LOCATE_ME.equals(action))
+			{
+				Log.d(TAG, String.format("Got location %d, %d", intent.getIntExtra(LocationService.LAT, -1), intent.getIntExtra(LocationService.LON, -1)));
 			}
 		}
 	}
 
-	private boolean isServiceRunning(Class<?> serviceClass) {
+	private boolean isServiceRunning(Class<?> serviceClass)
+	{
 		ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-			if (serviceClass.getName().equals(service.service.getClassName())) { return true; }
+		for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE))
+		{
+			if (serviceClass.getName().equals(service.service.getClassName()))
+			{
+				return true;
+			}
 		}
 		return false;
 	}
